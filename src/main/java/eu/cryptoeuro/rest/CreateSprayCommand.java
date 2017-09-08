72c6e99685485dcce2b51e95095a7809e0690371
Julia Melnychuk <julia.melnychuk@transferwise.com>
2017-09-08 23:35:31 +0300

    Refactor imports

--- src/main/java/eu/cryptoeuro/rest/CreateSprayCommand.java
+++ src/main/java/eu/cryptoeuro/rest/CreateSprayCommand.java
@@ -1,15 +1,11 @@
 package eu.cryptoeuro.rest;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.validation.constraints.AssertTrue;
-
-import lombok.Data;
 
 @Getter
 @Setter
